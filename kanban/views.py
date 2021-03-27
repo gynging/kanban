9e9fb3fdcefa57d124470a441e75cc17bf1c41d4
森本眞一郎 <shin@MacBook-Pro.local>
2021-03-27 19:52:53 +0900

    3-1 done

--- kanban/views.py
+++ kanban/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import login # 追加
+from django.contrib.auth.forms import UserCreationForm # 追加
+from django.shortcuts import render, redirect # redirectをインポート
 
-# Create your views here.
 
-from django.shortcuts import render
 def index(request):
-    return render(request, "kanban/index.html") # このように変更する
+    return render(request, "kanban/index.html")
 
+
+def home(request):
+    return render(request, "kanban/home.html")
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user_instance = form.save()
+            login(request, user_instance)
+            return redirect("kanban:home")
+    else:
+        form = UserCreationForm()
+
+    context = {
+        "form": form
+    }
+    return render(request, 'kanban/signup.html', context)
