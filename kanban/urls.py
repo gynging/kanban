47d817e50500b3b9b09a0fed8fad260247b76b99
森本眞一郎 <shin@MacBook-Pro.local>
2021-03-31 21:58:20 +0900

    4-2

--- kanban/urls.py
+++ kanban/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("home/", views.HomeView.as_view(), name="home"),
     path("signup/", views.signup, name="signup"),
-    path("users/<int:pk>/", views.UserDetailView.as_view(), name="users_detail"), # 追加
+    path("users/<int:pk>/", views.UserDetailView.as_view(), name="users_detail"),
+    path("users/<int:pk>/update/", views.UserUpdateView.as_view(), name="users_update"), # 追加
 ]
