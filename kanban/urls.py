7c6c3f352a9f62d791a7aafed0f41a13d400556f
森本眞一郎 <shin@MacBook-Pro.local>
2021-04-17 10:38:32 +0900

    5-2.2

--- kanban/urls.py
+++ kanban/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path("users/<int:pk>/update/", views.UserUpdateView.as_view(), name="users_update"),
     path("lists/", views.ListListView.as_view(), name="lists_list"),
     path("lists/create/", views.ListCreateView.as_view(), name="lists_create"), # 追加
+    path("lists/<int:pk>/", views.ListDetailView.as_view(), name="lists_detail"),
+
 ]
 
--- kanban/views.py
+++ kanban/views.py
@@ -1,19 +1,64 @@
-...
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin # 追加
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, resolve_url
+from django.views.generic import DetailView, UpdateView
 from django.urls import reverse_lazy # 追加
 from django.views.generic import DetailView, UpdateView, CreateView,ListView # 追加
-
 from .forms import UserForm, ListForm # 追加
-from . models import List # 追加
-...
+from .models import List # 追加
+from .forms import UserForm
+from .mixins import OnlyYouMixin
 
-...
+def index(request):
+    return render(request, "kanban/index.html")
+@login_required
+
+def home(request):
+    return render(request, "kanban/home.html")
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user_instance = form.save()
+            login(request, user_instance)
+            return redirect("kanban:home")
+    else:
+        form = UserCreationForm()
+        context = {
+            "form": form
+        }
+    return render(request, 'kanban/signup.html', context)
+
 class ListCreateView(LoginRequiredMixin, CreateView):
     model = List
     template_name = "kanban/lists/create.html"
     form_class = ListForm
-    success_url = reverse_lazy("kanban:home")
+    success_url = reverse_lazy("kanban:lists_list")
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+class UserDetailView(LoginRequiredMixin, DetailView): # この行を編集
+    model = User
+    template_name = "kanban/users/detail.html"
+
+class UserUpdateView(OnlyYouMixin, UpdateView):
+    model = User
+    template_name = "kanban/users/update.html"
+    form_class = UserForm
+
+    def get_success_url(self):
+        return resolve_url('kanban:users_detail', pk=self.kwargs['pk'])
+
+class ListListView(LoginRequiredMixin, ListView):
+    model = List
+    template_name = "kanban/lists/list.html"
+
+class ListDetailView(LoginRequiredMixin, DetailView):
+    model = List
+    template_name = "kanban/lists/detail.html"
